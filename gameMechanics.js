75ac92e45c1a2fcf5684c860793e75d40d67a667
David Zou <david.zou@outlook.com>
2016-12-05 18:54:11 -0800

    Cleaned up the main page further.

--- gameMechanics.js
+++ gameMechanics.js
@@ -33,6 +33,7 @@ var printGrid = function(inputGrid, gridName) {
 var chooseButton = function (currentGrid, gridName, selectedColumn, playerFlag) {
 	var newGrid = insertDisc(currentGrid, selectedColumn, playerFlag);
 	printGrid(newGrid, gridName);
+	printTurn(turn);
 }
 
 var insertDisc = function(currentGrid, selectedColumn, currentPlayer) {
@@ -55,30 +56,17 @@ var insertDisc = function(currentGrid, selectedColumn, currentPlayer) {
 		alert("This column is already filled, please try another column.");
 	}
 	else {
-		//if (currentPlayer == 1) {
 		if (turn % 2 == 0) {
-		//	currentGrid[selectedColumn][position] = playerOne;
 			currentGrid[selectedColumn][position] = playerTwo;
 		}
 		else {
-		//	currentGrid[selectedColumn][position] = playerTwo;
 			currentGrid[selectedColumn][position] = playerOne;
 		}
-	//turn = alternateTurns(currentPlayer);
 		turn++;
 		winCheck (currentGrid, selectedColumn, position, currentPlayer);
 		checkDraw(turn, height, width);
 	}
 	return currentGrid;
-}
-
-var alternateTurns = function(currentTurn) {
-	var playerOne = 0;
-	var playerTwo = 1;
-	if (currentTurn == playerOne) {
-		return playerTwo;
-	}
-	return playerOne;
 }
 
 var checkPlayer = function(currentPlayer) {
@@ -97,16 +85,11 @@ var checkDraw = function(currentTurn, gridHeight, gridWidth) {
 }
 
 var winCheck = function(currentGrid, selectedColumn, currentPosition, currentPlayer) {
-	//console.log("Current Player:" + " " + checkPlayer(currentPlayer));
 	var sum = 0;
 	var currentSumHorizontal = 1 + checkLeftHoriz(currentGrid, selectedColumn, currentPosition, currentPlayer, sum) + checkRightHoriz(currentGrid, selectedColumn, currentPosition, currentPlayer, sum);
 	var currentSumMainDiagonal = 1 + checkUpperLeftDiag(currentGrid, selectedColumn, currentPosition, currentPlayer, sum) + checkLowerRightDiag(currentGrid, selectedColumn, currentPosition, currentPlayer, sum);
     var currentSumAntiDiagonal = 1 + checkLowerLeftDiag(currentGrid, selectedColumn, currentPosition, currentPlayer, sum) + checkUpperRightDiag(currentGrid, selectedColumn, currentPosition, currentPlayer, sum);
 	var currentSumVertical = 1 + checkVertical(currentGrid, selectedColumn, currentPosition, currentPlayer, sum);
-	//console.log("Current Sum - Horizontal:" + " " + currentSumHorizontal);
-	//console.log("Current Sum - Main Diagonal:" + " " + currentSumMainDiagonal);
-	//console.log("Current Sum - Anti Diagonal:" + " " + currentSumAntiDiagonal);
-	//console.log("Current Sum - Vertical:" + " " + currentSumVertical);
 	if (currentSumHorizontal >= 4 || currentSumMainDiagonal >= 4 || currentSumAntiDiagonal >= 4 || currentSumVertical >= 4) {
 		if (currentPlayer % 2 == 0) {
 			alert ("Player Two Wins!");
@@ -117,40 +100,30 @@ var winCheck = function(currentGrid, selectedColumn, currentPosition, currentPla
 			scorePlayerOne++;
 		}
 		changeButtons();
-		drawScore(scorePlayerOne, scorePlayerTwo);
+		printScore(scorePlayerOne, scorePlayerTwo);
 	}
 }
 
 var checkLeftHoriz = function(currentGrid, selectedColumn, currentRow, currentPlayer, currentSum) {
 	var newSum = currentSum;
-	//console.log("Current Column of Left Horizontal:" + " " + selectedColumn);
-	//console.log("Current Token for Left Horizontal:" + " " + currentGrid[selectedColumn][currentPosition]);
 	if (selectedColumn == 0) return currentSum;
 	var newColumn = selectedColumn - 1;
-	//console.log("Next Column of Left Horizontal:" + " " + currentColumn);
-	//console.log("Next Token for Left Horizontal:" + " " + currentGrid[currentColumn][currentPosition]);
 	if ((newColumn >= 0) && (currentGrid[newColumn][currentRow] == checkPlayer(currentPlayer))) {
 		newSum++;
 		return checkLeftHoriz(currentGrid, newColumn, currentRow, currentPlayer, newSum);
 	}
-	//console.log("Sum of Left Horizontal:" + " " + newSum);
 	return newSum;
 }
 
 var checkRightHoriz = function(currentGrid, selectedColumn, currentRow, currentPlayer, currentSum) {
 	var newSum = currentSum;
     var width = currentGrid.length;
-    //console.log("Current Column of Right Horizontal:" + " " + selectedColumn);
-    //console.log("Current Token for Right Horizontal:" + " " + currentGrid[selectedColumn][currentPosition]);
 	if (selectedColumn == width - 1) return currentSum;
 	var newColumn = selectedColumn + 1;
-	//console.log("Next Column of Right Horizontal:" + " " + currentColumn);
-	//console.log("Next Token for Right Horizontal:" + " " + currentGrid[currentColumn][currentPosition]);
 	if ((newColumn < width) && (currentGrid[newColumn][currentRow] == checkPlayer(currentPlayer))) {
 		newSum++;
 		return checkRightHoriz(currentGrid, newColumn, currentRow, currentPlayer, newSum);
 	}
-	//console.log("Sum of Right Horizontal:" + " " + newSum);
 	return newSum;
 }
 
@@ -158,23 +131,15 @@ var checkLowerLeftDiag = function(currentGrid, selectedColumn, currentRow, curre
 	var newSum = currentSum;
     var height = currentGrid[selectedColumn].length;
     var width = currentGrid.length;
-    //console.log("Current Row of LowerLeftDiag:" + " " + currentRow);
-    //console.log("Current Column of LowerLeftDiag:" + " " + selectedColumn);
-    //console.log("Current Token for LowerLeftDiag:" + " " + checkPlayer(currentPlayer));
 	if ((currentRow == height - 1) || (selectedColumn == 0)) {
-	//	console.log("Hit LowerLeftDiag Max.");
 		return newSum;
 	}
 	var newRow = currentRow + 1;
 	var newColumn = selectedColumn - 1;
-	//console.log("Next Row of LowerLeftDiag:" + " " + newRow);
-	//console.log("Next Column of LowerLeftDiag:" + " " + newColumn);
-	//console.log("Next Token for LowerLeftDiag:" + " " + currentGrid[newColumn][newRow]);
 	if ((newRow < height) && (newColumn >= 0) && (currentGrid[newColumn][newRow] == checkPlayer(currentPlayer))) {
 		newSum++;
 		return checkLowerLeftDiag(currentGrid, newColumn, newRow, currentPlayer, newSum);
 	}
-	//console.log("Sum of LowerLeftDiag:" + " " + newSum);
 	return newSum;
 }
 
@@ -182,23 +147,15 @@ var checkUpperLeftDiag = function(currentGrid, selectedColumn, currentRow, curre
 	var newSum = currentSum;
     var height = currentGrid[selectedColumn].length;
     var width = currentGrid.length;
-    //console.log("Current Row of UpperLeftDiag:" + " " + currentRow);
-    //console.log("Current Column of UpperLeftDiag:" + " " + selectedColumn);
-    //console.log("Current Token for UpperLeftDiag:" + " " + checkPlayer(currentPlayer));
 	if ((currentRow == 0) || (selectedColumn == 0)) {
-	//	console.log("Hit UpperLeftDiag Max.");
 		return newSum;
 	}
 	var newRow = currentRow - 1;
 	var newColumn = selectedColumn - 1;
-	//console.log("Next Row of UpperLeftDiag:" + " " + newRow);
-	//console.log("Next Column of UpperLeftDiag:" + " " + newColumn);
-	//console.log("Next Token for UpperLeftDiag:" + " " + currentGrid[newColumn][newRow]);
 	if ((newRow < height) && (newColumn >= 0) && (currentGrid[newColumn][newRow] == checkPlayer(currentPlayer))) {
 		newSum++;
 		return checkUpperLeftDiag(currentGrid, newColumn, newRow, currentPlayer, newSum);
 	}
-	//console.log("Sum of UpperLeftDiag:" + " " + newSum);
 	return newSum;
 }
 
@@ -206,24 +163,15 @@ var checkLowerRightDiag = function(currentGrid, selectedColumn, currentRow, curr
 	var newSum = currentSum;
     var height = currentGrid[selectedColumn].length;
     var width = currentGrid.length;
-    //console.log("Current Row of LowerRightDiag:" + " " + currentRow);
-    //console.log("Current Column of LowerRightDiag:" + " " + selectedColumn);
-    //console.log("Current Token for LowerRightDiag:" + " " + checkPlayer(currentPlayer));
 	if ((currentRow == height - 1) || (selectedColumn == width - 1)) {
-	//	console.log("Hit LowerRightDiag Max.");
 		return newSum;
 	}
 	var newRow = currentRow + 1;
 	var newColumn = selectedColumn + 1;
-	//console.log("Next Row of LowerRightDiag:" + " " + newRow);
-	//console.log("Next Column of LowerRightDiag:" + " " + newColumn);
-	//console.log("Next Token for LowerRightDiag:" + " " + currentGrid[newColumn][newRow]);
 	if ((newRow < height) && (newColumn < width) && (currentGrid[newColumn][newRow] == checkPlayer(currentPlayer))) {
 		newSum++;
-	//	console.log("Adding to newSum in LowerRightDiag.");
 		return checkLowerRightDiag(currentGrid, newColumn, newRow, currentPlayer, newSum);
 	}
-	//console.log("Sum of LowerRightDiag:" + " " + newSum);
 	return newSum;
 }
 
@@ -231,40 +179,27 @@ var checkUpperRightDiag = function(currentGrid, selectedColumn, currentRow, curr
 	var newSum = currentSum;
     var height = currentGrid[selectedColumn].length;
     var width = currentGrid.length;
-    //console.log("Current Row of UpperRightDiag:" + " " + currentRow);
-    //console.log("Current Column of UpperRightDiag:" + " " + selectedColumn);
-    //console.log("Current Token for UpperRightDiag:" + " " + checkPlayer(currentPlayer));
 	if ((currentRow == 0) || (selectedColumn == width - 1)) {
-	//	console.log("Hit UpperRightDiag Max.");
 		return newSum;
 	}
 	var newRow = currentRow - 1;
 	var newColumn = selectedColumn + 1;
-	//console.log("Next Row of UpperRightDiag:" + " " + newRow);
-	//console.log("Next Column of UpperRightDiag:" + " " + newColumn);
-	//console.log("Next Token for UpperRightDiag:" + " " + currentGrid[newColumn][newRow]);
 	if ((newRow >= 0) && (newColumn < width) && (currentGrid[newColumn][newRow] == checkPlayer(currentPlayer))) {
 		newSum++;
 		return checkUpperRightDiag(currentGrid, newColumn, newRow, currentPlayer, newSum);
 	}
-	//console.log("Sum of UpperRightDiag:" + " " + newSum);
 	return newSum;
 }
 
 var checkVertical = function(currentGrid, selectedColumn, currentRow, currentPlayer, currentSum) {
 	var newSum = currentSum;
     var height = currentGrid[selectedColumn].length;
-    //console.log("Current Row of Vertical:" + " " + currentRow);
-    //console.log("Current Token for Vertical:" + " " + checkPlayer(currentPlayer));
 	if (currentRow == height - 1) return currentSum;
 	var newRow = currentRow + 1;
-	//console.log("Next Row of Vertical:" + " " + newRow);
-	//console.log("Next Token for Vertical:" + " " + currentGrid[selectedColumn][newRow]);
 	if ((newRow < height) && (currentGrid[selectedColumn][newRow] == checkPlayer(currentPlayer))) {
 		newSum++;
 		return checkVertical(currentGrid, selectedColumn, newRow, currentPlayer, newSum);
 	}
-	//console.log("Sum of Vertical:" + " " + newSum);
 	return newSum;
 }
 
@@ -274,6 +209,7 @@ var resetGame = function(currentGrid) {
 	mainGrid = createGrid(height,width);
 	printGrid(mainGrid, "mainGrid");
 	turn = 1;
+	printTurn(turn);
 	document.getElementById("buttonA").disabled = false;
 	document.getElementById("buttonB").disabled = false;
 	document.getElementById("buttonC").disabled = false;
@@ -295,8 +231,16 @@ var changeButtons = function() {
 	document.getElementById("reset").value = "Play Again!";	
 }
 
-var drawScore = function(scorePlayerOne, scorePlayerTwo) {
-	document.getElementById("scoreBoard").innerHTML = "<b>Score Board:</b><br>Player One:" + " " + scorePlayerOne + "<br>Player Two:" + " " + scorePlayerTwo;
+var printScore = function(scorePlayerOne, scorePlayerTwo) {
+	document.getElementById("scoreBoard").innerHTML = "<b>Score Board:</b><br>Player One:" + " " + scorePlayerOne + "<br><font color='red'>Player Two:" + " " + scorePlayerTwo + "</font><br>";
+}
+
+var printTurn = function (turn) {
+	var declareTurn = "Player One (X)";
+	if (turn % 2 == 0) {
+		declareTurn = "<font color='red'>Player Two (O)</font>";
+	}
+	document.getElementById("currentTurn").innerHTML = "Current Turn:" + " " + declareTurn + " ";
 }
 
 // Test Functions
